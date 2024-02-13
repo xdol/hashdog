2ce049ab5b66fb73f685a9c8ed21a4953d72c8ba
Ada <ada@gnous.eu>
2024-02-13 23:11:37 +0100

    fix(config): fix bug in structure
    Fix: Wordlist config used Rainbow config
    Fix: Test for test configure if bug reappears

--- internal/bruteforce/config.go
+++ internal/bruteforce/config.go
@@ -3,12 +3,13 @@ package bruteforce
 import (
 	"git.xdol.org/xdol/hashdog/internal/bruteforce/charset"
 	"git.xdol.org/xdol/hashdog/internal/bruteforce/rainbow"
+	"git.xdol.org/xdol/hashdog/internal/bruteforce/wordlist"
 )
 
 type Config struct {
 	Length   uint8
 	Threads  uint8
 	Rainbow  rainbow.Config
-	Wordlist rainbow.Config
+	Wordlist wordlist.Config
 	Charset  charset.Config
 }
